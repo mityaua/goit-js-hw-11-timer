071386e18a512082695f42e5e9241ac1e8155dfe
Dmytro <mityaua@gmail.com>
2021-01-13 21:52:26 +0200

    Добавляет и апргрейдит таймер через class

--- src/js/timer.js
+++ src/js/timer.js
@@ -1,76 +1,154 @@
-// Цифры от таймера в DOM
-const daysRef = document.querySelector('[data-value="days"]');
-const hoursRef = document.querySelector('[data-value="hours"]');
-const minsRef = document.querySelector('[data-value="mins"]');
-const secsRef = document.querySelector('[data-value="secs"]');
+// Декларация класса
+class NewTimer {
+  constructor({ selector, targetDate, intervalId = null }) {
+    (this.selector = selector),
+      (this.targetDate = targetDate),
+      (this.intervalId = intervalId);
+  }
 
-// Обьект с методами
-const timer = {
-  intervalId: null,
-
+  // Запускает отсчёт
   start() {
-    const targetDate = new Date(2022, 0, 1, 0, 0, 0);
-
-    updateTimer(0);
+    this.updateTimer(0);
 
     this.intervalId = setInterval(() => {
       const currentTime = Date.now();
-      const deltaTime = targetDate - currentTime;
+      const deltaTime = this.targetDate - currentTime;
 
-      updateTimer(deltaTime);
+      this.updateTimer(deltaTime);
 
       if (deltaTime <= 0) {
-        alert('С Новым годом! 🎄');
         this.stop();
       }
     }, 1000);
-  },
+  }
 
+  // Останавливает отсчёт
   stop() {
     clearInterval(this.intervalId);
     this.intervalId = null;
-    updateTimer(0);
-  },
-};
+    this.updateTitle();
+    this.updateTimer(0);
+  }
 
-// Запускает отсчёт
-timer.start();
+  // Обновляет таймер
+  updateTimer(time) {
+    const days = Math.floor(time / (1000 * 60 * 60 * 24));
+    const hours = this.pad(
+      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+    );
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
 
-// Обновляет таймер
-function updateTimer(time) {
-  /*
-   * Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество
-   * миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
-   */
-  const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const timerRef = document.querySelector('#timer-1');
+    const daysRef = timerRef.querySelector('[data-value="days"]');
+    const hoursRef = timerRef.querySelector('[data-value="hours"]');
+    const minsRef = timerRef.querySelector('[data-value="mins"]');
+    const secsRef = timerRef.querySelector('[data-value="secs"]');
 
-  /*
-   * Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
-   * остатка % и делим его на количество миллисекунд в одном часе
-   * (1000 * 60 * 60 = миллисекунды * минуты * секунды)
-   */
-  const hours = pad(
-    Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
-  );
+    daysRef.textContent = `${days}`;
+    hoursRef.textContent = `${hours}`;
+    minsRef.textContent = `${mins}`;
+    secsRef.textContent = `${secs}`;
+  }
 
-  /*
-   * Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
-   * миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
-   */
-  const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+  // Корректирует числовой формат
+  pad(value) {
+    return String(value).padStart(2, '0');
+  }
 
-  /*
-   * Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
-   * миллисекунд в одной секунде (1000)
-   */
-  const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
-
-  daysRef.textContent = `${days}`;
-  hoursRef.textContent = `${hours}`;
-  minsRef.textContent = `${mins}`;
-  secsRef.textContent = `${secs}`;
+  // Обновляет контент
+  updateTitle() {
+    const titleRef = document.querySelector('#title');
+    alert('С Новым годом! 🥳');
+    titleRef.textContent = 'Happy New Year 2022! 🥳';
+  }
 }
 
-function pad(value) {
-  return String(value).padStart(2, '0');
-}
+// Конструктор класса
+const newYearTimer = new NewTimer({
+  selector: '#timer-1',
+  targetDate: new Date(2022, 0, 1, 0, 0, 0),
+});
+
+// Запускает отсчёт немедленно
+newYearTimer.start();
+
+// Первый вариант через обьект
+
+// // Цифры от таймера в DOM
+// const daysRef = document.querySelector('[data-value="days"]');
+// const hoursRef = document.querySelector('[data-value="hours"]');
+// const minsRef = document.querySelector('[data-value="mins"]');
+// const secsRef = document.querySelector('[data-value="secs"]');
+// const titleRef = document.querySelector('#title');
+
+// // Обьект с методами
+// const timer = {
+//   intervalId: null,
+
+//   start() {
+//     const targetDate = new Date(2022, 0, 1, 0, 0, 0);
+
+//     updateTimer(0);
+
+//     this.intervalId = setInterval(() => {
+//       const currentTime = Date.now();
+//       const deltaTime = targetDate - currentTime;
+
+//       updateTimer(deltaTime);
+
+//       if (deltaTime <= 0) {
+//         alert('С Новым годом! 🎄');
+//         this.stop();
+//       }
+//     }, 1000);
+//   },
+
+//   stop() {
+//     clearInterval(this.intervalId);
+//     this.intervalId = null;
+//     updateTimer(0);
+//   },
+// };
+
+// // Запускает отсчёт
+// timer.start();
+
+// // Обновляет таймер
+// function updateTimer(time) {
+//   /*
+//    * Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество
+//    * миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
+//    */
+//   const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+
+//   /*
+//    * Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
+//    * остатка % и делим его на количество миллисекунд в одном часе
+//    * (1000 * 60 * 60 = миллисекунды * минуты * секунды)
+//    */
+//   const hours = pad(
+//     Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+//   );
+
+//   /*
+//    * Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
+//    * миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
+//    */
+//   const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+
+//   /*
+//    * Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
+//    * миллисекунд в одной секунде (1000)
+//    */
+//   const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+
+//   daysRef.textContent = `${days}`;
+//   hoursRef.textContent = `${hours}`;
+//   minsRef.textContent = `${mins}`;
+//   secsRef.textContent = `${secs}`;
+// }
+
+// function pad(value) {
+//   return String(value).padStart(2, '0');
+// }
