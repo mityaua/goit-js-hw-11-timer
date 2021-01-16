357c566b70b70bd5966ab5f80c83a82eebd886cb
Dmytro <mityaua@gmail.com>
2021-01-16 14:54:29 +0200

    Fix snow bug in npm package

--- src/js/snow.js
+++ src/js/snow.js
@@ -4,3 +4,10 @@ snowf.init({
   size: 5,
   amount: 50,
 });
+
+// Сохраняет пропорции при смене экрана (костыль для бага пакета)
+window.onresize = function () {
+  setTimeout(() => {
+    location.reload();
+  }, 500);
+};
